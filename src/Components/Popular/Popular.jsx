eaf6ae1e987f01ff9883bf889060149b6272c2ce
hello <mav@aurion.taild8fcc.ts.net>
2025-02-19 22:43:37 +0530

    Updated stuff

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import Item from "../Item/Item"; // Updated import
+import products from "../Assets/data";
 
 const Popular = () => {
-  // Your component logic here
-  return <div>{/* Your component JSX */}</div>;
+  return (
+    <div className="popular">
+      <h1>POPULAR</h1>
+      <hr />
+      <div className="popular-item">
+        {products.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Popular;
